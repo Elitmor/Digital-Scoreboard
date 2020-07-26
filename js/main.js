04cd71adb6a66dfe959aa5239e125a7bd178c877
elitmor <elitmor@gmail.com>
2020-07-26 11:49:59 +0300

    added Digital Scoreboard

---
+++ js/main.js
@@ -0,0 +1,27 @@
+var teamA = 0;
+var teamB = 0;
+
+function result(points, team) {
+    team = "team" + (team ? "B" : "A");
+    if (window[team] + points >= 0) window[team] += points;
+    document.querySelectorAll("p")[team == "teamA" ? 0 : 1].textContent = window[team];
+}
+
+document.querySelectorAll("section")[0].addEventListener("click", function(e) {
+    if (e.target.tagName == "DIV") result(parseInt(e.target.textContent), 0);
+});
+document.querySelectorAll("section")[1].addEventListener("click", function(e) {
+    if (e.target.tagName == "DIV") result(parseInt(e.target.textContent), 1);
+});
+
+document.querySelectorAll("h2")[0].addEventListener("click", function() {
+    document.execCommand("selectAll", false, null);
+});
+
+document.querySelectorAll("h2")[1].addEventListener("click", function() {
+    document.execCommand("selectAll", false, null);
+});
+
+document.addEventListener("keypress", function(e) {
+    if ((e.keyCode || e.which) == 13) return false;
+});
